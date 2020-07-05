41580fb8e4e1d40f89d0cc4af2d626479f47cd5d
Tim Wagner <tim.wagner@ibykus.de>
2020-07-05 18:12:05 +0200

    - die Klick-Funktion wurde registriert

--- app/src/main/java/io/github/hawaiiandroid/aboutme/MainActivity.kt
+++ app/src/main/java/io/github/hawaiiandroid/aboutme/MainActivity.kt
@@ -3,6 +3,7 @@ package io.github.hawaiiandroid.aboutme
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 
@@ -10,6 +11,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.done_button).setOnClickListener { addNickname(it) }
     }
 
     /**
